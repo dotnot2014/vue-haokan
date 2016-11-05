8631faa41afaa02029516365be8108c08316705e
“dotnot2014git config --global user.email “5168247@qq.com <5168247@qq.com>
2016-11-05 09:02:29 +0800

    add somthing

--- src/components/World.vue
+++ src/components/World.vue
@@ -1,45 +1,72 @@
 <template>
   <div class="world">
     <div>
-        <span class="bg-primary">{{msg}}</span><br>
-        <button class="ui button" @click="getdata">数据载入</button>
-        <br><br>
-        <hr>
+      <span class="bg-primary">{{msg}}</span>
+      <div class="ui horizontal divider">组件区</div>
+      <button class="ui button" @click="getdata">数据载入</button>
+      <br>
+      <div class="ui center aligned basic segment">
+        <div class="ui left icon action input">
+          <i class="search icon"></i>
+          <input type="text" placeholder="Order #">
+          <div class="ui blue submit button">搜索</div>
+        </div>
+      <br><br>
+      <div class="ui horizontal list">
+        <div class="item">
+          <img class="ui mini circular image" src="../../static/molly.png">
+          <div class="content">
+            <div class="ui sub header">Molly</div>Coordinator </div>
+        </div>
+        <div class="item">
+          <img class="ui mini circular image" src="../../static/molly.png">
+          <div class="content">
+            <div class="ui sub header">Elyse</div>Developer </div>
+        </div>
+        <div class="item">
+          <img src="../../static/molly.png" class="ui mini circular image">
+          <div class="content">
+            <div class="ui sub header">Eve</div>Project Manager </div>
+        </div>
+      </div>
+      </div>
+      <br>
+       <div class="ui horizontal divider">数据区</div>
+       <!--<div class="ui divider"></div>-->
     </div>
     <div class="content1">
-        <ul>
-            <li v-for="item in articles" >
-                <div class="ui card">
-                  <div class="content">
-                    <span class="right floated meta">14h</span>
-                    <span class="left floated meta">
-                        <img class="ui avatar image" src="../../static/logo.png"> Elliot
+      <ul>
+        <li v-for="item in articles">
+          <div class="ui card">
+            <div class="content">
+              <span class="right floated meta">14h</span>
+              <span class="left floated meta">
+                        <img class="ui avatar image" src="../../static/molly.png"> Elliot
                     </span>
-                  </div>
-                  <div class="image setheight">
-                    <img :src="item.coverUrl">
-                  </div>
-                  <div class="content">
-                    <span class="right floated">
+            </div>
+            <div class="image setheight">
+              <img :src="item.coverUrl">
+            </div>
+            <div class="content">
+              <span class="right floated">
                       <i class="heart outline like icon"></i>
                       17 likes
                     </span>
-                    <span class="left floated">
+              <span class="left floated">
                       <i class="comment icon"></i>
                       3 comments
                     </span>
-                  </div>
-                  <div class="extra content left">
-      
-                    <div class="ui large">
-                      <i class="heart outline icon"></i>
-                      {{ item.title }}
-                    </div>
-                  </div>
-                </div>
+            </div>
+            <div class="extra content left">
 
-            </li>
-        </ul>
+              <div class="ui large">
+                <i class="heart outline icon"></i> {{ item.title }}
+              </div>
+            </div>
+          </div>
+
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -102,73 +129,83 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.content1{
-  margin:0 auto;
-  width:500px;
-  background:#fdfdfd;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-  text-align: center;
-}
-
-li{
-  padding-top:15px;
-}
-
-a {
-  color: white;
-  text-decoration:none; 
-}
-.span2{
-  margin:0 auto;
-  display:block;
-  width:96%;
-  height:80px;
-  text-align:left;
-  background:white;
-  color:gray;
-}
-.setheight{
+  .content1 {
+    margin: 0 auto;
+    width: 500px;
+    background: #fdfdfd;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+    text-align: center;
+  }
+  
+  li {
+    padding-top: 15px;
+  }
+  
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  
+  .span2 {
+    margin: 0 auto;
+    display: block;
+    width: 96%;
+    height: 80px;
+    text-align: left;
+    background: white;
+    color: gray;
+  }
+  
+  .setheight {
     height: 290px;
-}
-.title{
-  padding:10px;
-  text-align:left;
-}
-.divid{
-  padding:0;
-}
-.postfrom{
-  color:#999;
-  font-size:12px;
-  text-align:left;
-  padding:10px;
-  width:220px;
-}
-.line{
-  margin:0;
-  padding:0;
-  border-bottom:1px solid #ededed;
-  width:100%;
-}
-.card{
-  margin: 30px;
-  width:440px;
-  border-top-left-radius:10px;
-  border-top-right-radius:10px;
-}
-a{
-  color:#555;
-}
-.alert-icon-float-left {
-  font-size:32px;
-  float:left;
-  margin-right:5px;
-}
-.left{
-  text-align:left;
-}
+  }
+  
+  .title {
+    padding: 10px;
+    text-align: left;
+  }
+  
+  .divid {
+    padding: 0;
+  }
+  
+  .postfrom {
+    color: #999;
+    font-size: 12px;
+    text-align: left;
+    padding: 10px;
+    width: 220px;
+  }
+  
+  .line {
+    margin: 0;
+    padding: 0;
+    border-bottom: 1px solid #ededed;
+    width: 100%;
+  }
+  
+  .card {
+    margin: 30px;
+    width: 440px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+  
+  a {
+    color: #555;
+  }
+  
+  .alert-icon-float-left {
+    font-size: 32px;
+    float: left;
+    margin-right: 5px;
+  }
+  
+  .left {
+    text-align: left;
+  }
 </style>
